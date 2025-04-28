a1909b410834e1e6c07d8ba4c80a269095f61078
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-28 15:23:26 +0200

    feat: base

---
+++ script.js
@@ -0,0 +1,8 @@
+const titolo= document.getElementById("titolo")
+console.log(titolo)
+
+const image = document.getElementById("img")
+console.log(image)
+
+const button = document.getElementById("btn");
+console.log(button)
