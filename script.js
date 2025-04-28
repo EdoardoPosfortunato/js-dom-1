dfcd74233133d938515cad15d416bd0be97f79e2
EdoardoPosfortunato <edoardo.posfortunato27@gmail.com>
2025-04-28 15:55:55 +0200

    feat: esercizio

--- script.js
+++ script.js
@@ -6,3 +6,18 @@ console.log(image)
 
 const button = document.getElementById("btn");
 console.log(button)
+
+
+button.addEventListener ('click', 
+    function() {
+        if (button.innerText === 'Accendi') {
+            image.src = '/img/yellow_lamp 1.png'
+            button.innerText = "Spegni"
+        } else
+
+        if (button.innerText === 'Spegni') {
+            image.src = '/img/white_lamp 1.png'
+            button.innerText = "Accendi"
+        }
+    }
+)
